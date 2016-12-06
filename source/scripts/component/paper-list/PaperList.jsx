0bc85476257ef0967c9e11e180cab6c32dc4bae5
rangel <18792561236@163.com>
2016-12-06 16:04:27 +0800

    add paperList style

--- source/scripts/component/paper-list/PaperList.jsx
+++ source/scripts/component/paper-list/PaperList.jsx
@@ -3,6 +3,8 @@ require('font-awesome');
 var Reflux = require('reflux');
 var PaperListAction = require('../../actions/paper-list/paper-list');
 var PaperListStore = require('../../store/paper-list/paper-list');
+var {ProgressBar} = require('react-bootstrap');
+
 
 var PaperList = React.createClass({
 
@@ -21,15 +23,29 @@ var PaperList = React.createClass({
               <span>{paper.programId}</span>
             </h4>
             <div>
-              <ul className="list">
+              <ul className="list row">
                 {paper.data.map((item, index) => {
                   return (
-                      <li className={"list-item"} key={index}>
-                        <a href="#">
-                          <span className="paper-name">
-                            {item.paperName}
+                      <li className="list-item col-sm-2" key={index}>
+                        <div className="icon-box col-sm-5">
+                          <span>
+                            <i className="fa fa-files-o"> </i>
                           </span>
-                        </a>
+                        </div>
+                        <div className="paper-box col-sm-7">
+                          <div>
+                            <a href="#">
+                            <span className="paper-name">
+                            {item.paperName}
+                            </span>
+                            </a>
+                          </div>
+                          <span className="quizzes">30道题</span>
+                          <div className="progress">
+                            <div className="progress-bar"></div>
+                          </div>
+                          <span className="progress-description">50% finished</span>
+                        </div>
                       </li>
                   )
                 })}

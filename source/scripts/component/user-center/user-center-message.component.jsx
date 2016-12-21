cb7580b200c551851a0ba24d840d8cd0b5dd1689
Aym-fuhong <18829287015@163.com>
2016-12-21 15:53:45 +0800

    add student message center

--- source/scripts/component/user-center/user-center-message.component.jsx
+++ source/scripts/component/user-center/user-center-message.component.jsx
@@ -1,4 +1,5 @@
 'use strict';
+import '../../../less/user-center.less';
 var UserCenterStore = require('../../store/user-center/user-center-store');
 var Reflux = require('reflux');
 
@@ -11,12 +12,64 @@ var Message = React.createClass({
       currentState: 'userDetail'
     };
   },
-render:function () {
+  render: function () {
     var classString = (this.state.currentState === 'message' ? '' : ' hide');
 
     return (
-        <div className={"row container-fluid"+classString}>
-          message
+        <div className={"col-md-9 col-sm-9 col-xs-12 container-fluid" + classString}>
+          <div id="tabs" className="content">
+            <div>
+              <ul id="myTab" className="myTab nav nav-tabs">
+                <li className="active">
+                  <a href="#unread" data-toggle="tab">
+                    <font color="gray"><i className="fa fa-bell"></i>&nbsp;未读</font>
+                  </a>
+                </li>
+                <li><a href="#all" data-toggle="tab">
+                  <font color="gray"><i className="fa fa-envelope"></i>&nbsp;所有</font>
+                </a>
+                </li>
+              </ul>
+            </div>
+            <div id="myTabContent" className="row tab-content">
+              <div className="tab-pane fade in active" id="unread">
+                <table className="table table-bordered table-striped table-hover">
+                  <thead className="table-header">
+                  <th>未读消息</th>
+                  </thead>
+                  <tbody className="table-body">
+                  <tr>
+                  <td>教练未同意查看第一张试卷第一题的答案<i className="fa fa-check pull-right"></i></td>
+                  </tr>
+                  <tr>
+                    <td>教练未同意查看第一张试卷第二题的答案<i className="fa fa-check pull-right"></i></td>
+                  </tr>
+                  <tr>
+                    <td>教练未同意查看第一张试卷第三题的答案<i className="fa fa-check pull-right"></i></td>
+                  </tr>
+                  </tbody>
+                </table>
+              </div>
+              <div className="tab-pane fade" id="all">
+                <table className="table table-bordered table-striped table-hover col-sm-5 col-sm-offset-1">
+                  <thead className="table-header">
+                  <th>所有消息</th>
+                  </thead>
+                  <tbody className="table-body">
+                  <tr>
+                    <td>教练未同意查看第一张试卷第一题的答案<i className="fa fa-check pull-right"></i></td>
+                  </tr>
+                  <tr>
+                    <td>教练未同意查看第一张试卷第二题的答案<i className="fa fa-check pull-right"></i></td>
+                  </tr>
+                  <tr>
+                    <td>教练未同意查看第一张试卷第三题的答案<i className="fa fa-check pull-right"></i></td>
+                  </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
         </div>
     );
   }

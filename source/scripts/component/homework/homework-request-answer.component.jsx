059f8088f2fe55ff112f2be60c1642873f0472f5
bmeritter <bme_ritter@foxmail.com>
2016-12-26 10:25:10 +0800

    add request for answer

--- source/scripts/component/homework/homework-request-answer.component.jsx
+++ source/scripts/component/homework/homework-request-answer.component.jsx
@@ -1,12 +1,26 @@
 'use strict';
 
+var Reflux = require('reflux');
+var MessageActions = require('../../actions/messages/message-actions');
+var RequestAnswerStore = require('../../store/message/request-answer-store');
+
 var RequestAnswer = React.createClass({
+  mixins: [Reflux.connect(RequestAnswerStore)],
+
+  handleRequest: function () {
+    MessageActions.createMessage({
+      to: 1,
+      type: 'requestAnswer',
+      deeplink: 'papers/1/sections/1/homeworks/1'
+    });
+  },
+
   render() {
 
     return (
       <div className="runningResult tab">
         <div className="result">
-          <button className="btn btn-primary">请求答案</button>
+          <button className="btn btn-primary" onClick={this.handleRequest}>请求答案</button>
 
         </div>
       </div>
---
+++ source/scripts/store/message/request-answer-store.js
@@ -0,0 +1,24 @@
+/*eslint no-magic-numbers: 0*/
+'use strict';
+
+var Reflux = require('reflux');
+var superAgent = require('superagent');
+var MessageActions = require('../../actions/messages/message-actions');
+var errorHandler = require('../../../../tools/error-handler.jsx');
+
+var requestAnswerStore = Reflux.createStore({
+  listenables: [MessageActions],
+
+  onCreateMessage: function (data) {
+    superAgent
+      .post('/api/messages')
+      .use(errorHandler)
+      .send(data)
+      .end((err, res) => {
+        console.log(res.body)
+      })
+
+  }
+});
+
+module.exports = requestAnswerStore;

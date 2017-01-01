321fa28687c37b5ec72d9a74d6d7918e000a4d39
bmeritter <bme_ritter@foxmail.com>
2017-01-01 16:17:40 +0800

    fix eslint issue

--- tools/getQueryString.js
+++ tools/getQueryString.js
@@ -1,5 +1,5 @@
-function getQueryString(name) {
-  var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+function getQueryString (name) {
+  var reg = new RegExp('(^|&)' + name + '=([^&]*)(&|$)', 'i');
   var r = window.location.search.substr(1).match(reg);
   if (r != null) {
     return unescape(r[2]);

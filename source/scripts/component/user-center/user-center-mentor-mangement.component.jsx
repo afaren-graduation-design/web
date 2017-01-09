583b51556b02a15f063c5d65dcd39884f5f842ab
bmeritter <bme_ritter@foxmail.com>
2017-01-09 17:31:34 +0800

    fix a bug

--- source/scripts/component/user-center/user-center-mentor-mangement.component.jsx
+++ source/scripts/component/user-center/user-center-mentor-mangement.component.jsx
@@ -29,7 +29,7 @@ var MentorManagement = React.createClass({
       <div className={"col-md-9 col-sm-9 col-xs-12" + classString}>
         <div className="content">
           <MentorTable mentorList={this.state.mentorList}/>
-          <MentorSearch/>
+          <MentorSearch mentorList={this.state.mentorList}/>
         </div>
       </div>
     );

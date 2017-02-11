0220a4d7dda3e2889b06fa49c566c82b3a321e53
Yvette14 <libra1014tyb@163.com>
2017-02-11 13:28:15 +0800

    show mentor in request-answer page

--- source/scripts/component/homework/homework-request-answer.component.jsx
+++ source/scripts/component/homework/homework-request-answer.component.jsx
@@ -1,15 +1,27 @@
 'use strict';
 
 var Reflux = require('reflux');
+var MentorManagementStore = require('../../store/user-center/mentor-management-store');
+var MentorManagementAction = require('../../actions/user-center/mentor-management-action');
 var MessageActions = require('../../actions/messages/message-actions');
 var RequestAnswerStore = require('../../store/message/request-answer-store');
 
 var RequestAnswer = React.createClass({
-  mixins: [Reflux.connect(RequestAnswerStore)],
+  mixins: [Reflux.connect(MentorManagementStore), Reflux.connect(RequestAnswerStore)],
+
+  getInitialState: function () {
+    return {
+      mentorList: []
+    };
+  },
+
+  componentDidMount: function () {
+    MentorManagementAction.getMentors();
+  },
 
   handleRequest: function () {
     MessageActions.createMessage({
-      to: 1,
+      to: this.state.mentorList[0].userId,
       type: 'REQUEST_ANSWER',
       deeplink: `papers/${this.props.paperId}/sections/${this.props.sectionId}/homeworks/${this.props.orderId}`
     });
@@ -17,10 +29,14 @@ var RequestAnswer = React.createClass({
 
   render() {
     const RequestAnswerBtn = <button className="btn btn-primary" onClick={this.handleRequest}>请求答案</button>;
-    let result = this.props.answer ? this.props.answer : RequestAnswerBtn;
+    const MentorSpan = <span
+      className="mentor-span">{this.state.mentorList.length ? 'mentor : ' + this.state.mentorList[0].name : ''}</span>;
+    let info = this.state.mentorList.length ? '' : '您当前没有mentor,请先添加mentor.';
+    let result = info || (this.props.answer ? this.props.answer : RequestAnswerBtn);
     return (
       <div className="runningResult tab">
         <div className="result">
+          {MentorSpan}
           {result}
         </div>
       </div>

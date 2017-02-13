3ff09e1ebc72b7b14df59e4b4cd4248d3dcb05c9
Yvette14 <libra1014tyb@163.com>
2017-02-13 16:59:04 +0800

    page to user-center when does not  have mentor

--- source/scripts/component/homework/homework-request-answer.component.jsx
+++ source/scripts/component/homework/homework-request-answer.component.jsx
@@ -1,6 +1,7 @@
 'use strict';
 
 var Reflux = require('reflux');
+// var page = require('page');
 var MentorManagementStore = require('../../store/user-center/mentor-management-store');
 var MentorManagementAction = require('../../actions/user-center/mentor-management-action');
 var MessageActions = require('../../actions/messages/message-actions');
@@ -37,10 +38,11 @@ var RequestAnswer = React.createClass({
   },
 
   render() {
+    const AddMentor = <a href='user-center.html'>您当前没有mentor,请先添加mentor.</a>;
     const RequestAnswerBtn = <button className="btn btn-primary" onClick={this.handleRequest}>请求答案</button>;
     const MentorSpan = <span
       className="mentor-span">{this.state.mentorList.length ? 'mentor : ' + this.state.mentorList[0].name : ''}</span>;
-    let info = this.state.mentorList.length ? '' : '您当前没有mentor,请先添加mentor.';
+    let info = this.state.mentorList.length ? '' : AddMentor;
     let result = info || (this.state.answer ? this.state.answer : RequestAnswerBtn);
     return (
       <div className="runningResult tab">
